dde458629f7c6613c6f32f4052b44c20d8d5f403
DeFUCC <fond@fondfrukt.ru>
2020-09-13 22:52:02 +0300

    smooth

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 import { nadis, chakras, tattvas } from './tattvas.js'
 const vishva = {...tattvas, ...chakras, ...nadis}
 const svg = document.getElementById('santana')
-
+let active = null
 window.addEventListener('load', app, false)
 
 function app() {
-  let active = null
+  
   const svgDoc = svg.contentDocument
   const aham = svgDoc.getElementById('aham')
   const overlay = document.getElementById('overlay')
@@ -45,10 +45,19 @@ function app() {
         }
       }
       active = el
-      info.innerHTML = vishva[item].text
+      info.innerHTML = format(vishva[item])
       active.classList.add('active')
       aham.classList.add('has-active')
       overlay.classList.toggle('open')
     }
   }
+}
+
+function format(item) {
+  return /*html*/`
+    <p class="sanskrit">${item.sans}</p>
+    <p class="transcript">${item.trans}</p>
+    <h2>${item.title}</h2>
+    <p>${item.text}</p>
+  `
 }
